2c14f3bab8ab10df6856b139dfed2dd768835cae
Stas Makarov <jehaby@ya.ru>
2015-12-11 01:44:47 +0300

    Some mess

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 var DeliveryData = {
-    deliver_to: ['В офис', "Клиенту", "Партнеру"]
+    deliver_to: ['В офис', "Клиенту", "Партнеру"],
+    test: 'test'
 }
 
+test = 42;
 
 var Delivery = Vue.extend({
     props: ['data'],
     template: "\
 <div> \
 Delivery goes here!11 \
-<select>\
-<option v-for="data.deliver_to"></option>\
-</select>\
+<p> {{ data }} </p>\
+<small> last paragraph </small>\
+<pre>\
+{{ $props | json }}\
+</pre>\
 </div>\
 "
 });
@@ -19,12 +23,40 @@ Delivery goes here!11 \
 // Vue.component('delivery', Delivery);
 
 
-new Vue({
-    el: '#app',
+Vue.component('counter', {
+    template: "counter-template",
     data: {
-	message: 'Hello Vue.js!'
+        i: 0
     },
-    components: {
-	'delivery': Delivery
+    methods: {
+        count: function() {
+            this.i++;
+        }
     }
 })
+
+
+new Vue({
+    config: {
+        debug: true
+    },
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!',
+        data: DeliveryData,
+        i: 1
+    },
+    components: {
+        'delivery': Delivery
+    },
+    methods: {
+        handle: function () {
+            alert('Hi');
+        },
+        count: function () {
+            this.i = this.i * 2;
+        }
+    }
+});
+
+Vue.config.debug = true;
