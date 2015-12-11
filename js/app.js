be23742cc899878b5b34639a611ac43885137957
Stas Makarov <jehaby@ya.ru>
2015-12-11 21:58:54 +0300

    Before cleanup

--- js/app.js
+++ js/app.js
@@ -1,62 +1,44 @@
-var DeliveryData = {
-    deliver_to: ['В офис', "Клиенту", "Партнеру"],
-    test: 'test'
-}
-
-test = 42;
-
-var Delivery = Vue.extend({
-    props: ['data'],
-    template: "\
-<div> \
-Delivery goes here!11 \
-<p> {{ data }} </p>\
-<small> last paragraph </small>\
-<pre>\
-{{ $props | json }}\
-</pre>\
-</div>\
-"
+Vue.component('counter', {
+    template: "#counter-template",
+    data: function () {
+        return {
+            i: 0
+        }
+    },
+    props: ['text'],
+    methods: {
+        count: function() {
+            this.i += 1;
+        }
+    }
 });
 
 
-// Vue.component('delivery', Delivery);
 
+Vue.component('subscription', {
+    template: '#subscription-template',
+    data: function() {
+        currentPlan: 0
 
-Vue.component('counter', {
-    template: "counter-template",
-    data: {
-        i: 0
     },
     methods: {
-        count: function() {
-            this.i++;
-        }
+
     }
-})
+});
+
+
+Vue.component('subscription-element', {
+    template: '#subscription-element',
+    props: ['chosen', 'weight', 'title'],
+    data: function() {
+
+    },
+
+});
 
 
 new Vue({
-    config: {
-        debug: true
-    },
     el: '#app',
-    data: {
-        message: 'Hello Vue.js!',
-        data: DeliveryData,
-        i: 1
-    },
-    components: {
-        'delivery': Delivery
-    },
-    methods: {
-        handle: function () {
-            alert('Hi');
-        },
-        count: function () {
-            this.i = this.i * 2;
-        }
-    }
 });
 
 Vue.config.debug = true;
